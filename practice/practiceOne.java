4e64c3362affdb631f4d0f56721e48ff9ab90792
spicytacos25 <luispillasagua@uagateway.org>
2016-10-25 17:49:41 +0000

    Work on magpie activity 2

--- practice/practiceOne.java
+++ practice/practiceOne.java
@@ -1,5 +1,10 @@
-public class practiceOne{
+import java.util.Scanner;
+
+public class practiceInput{
     public static void main(String[] args){
-        System.out.println("It works!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a word");
+        String s = in.nextLine();
+        System.out.println("You entered: " + s);
     }
 }
